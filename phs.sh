701804e14d9fd5aa9e40d60789cff104d6465bce
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-06-24 10:53:51 -0400

    Update phs.sh

--- phs.sh
+++ phs.sh
@@ -1,4 +1,4 @@
-git remote add space https://huggingface.co/spaces/<OWNER>/rnv-color-mcp
+git remote add space https://huggingface.co/spaces/RNVizion/rnv-color-mcp
 git add -A
 git commit -m "Add Docker frontmatter for HF Space"
 git push space HEAD:main --force
